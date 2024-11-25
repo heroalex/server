7000354819e10ff80a93234a7ef24c0a899405fa
heroalex <heroalex@gmx.de>
2024-11-25 20:26:43 +0100

    Update create_server.sh

--- scripts/create_server.sh
+++ scripts/create_server.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Check if all required arguments are provided
-if [ $# -ne 9 ]; then
-    echo "Usage: $0 <firewall_id> <server_name> <public_ipv4_id> <ssh_key_id> <username> <ssh_public_key> <wireguard_private_key> <wireguard_public_key> <api_token>"
-    exit 1
-fi
-
 # Hetzner API endpoint
 API_URL="https://api.hetzner.cloud/v1/servers"
 
@@ -16,22 +10,22 @@ payload=$(cat <<EOF
   "datacenter": "nbg1-dc3",
   "firewalls": [
     {
-      "firewall": "$1"
+      "firewall": ${FW_ID}
     }
   ],
   "image": "ubuntu-22.04",
-  "name": "$2",
+  "name": "${server_name}",
   "public_net": {
     "enable_ipv4": true,
     "enable_ipv6": false,
-    "ipv4": $3
+    "ipv4": ${IP_ID}
   },
   "server_type": "cx22",
   "ssh_keys": [
-    $4
+    ${SSH_ID}
   ],
   "start_after_create": true,
-  "user_data": "#cloud-config\nusers:\n  - name: $5\n    sudo: ['ALL=(ALL) NOPASSWD:ALL']\n    shell: /bin/bash\n    ssh_authorized_keys:\n      - \"$6\"\n\npackage_upgrade: true\npackages:\n  - wireguard\n  - podman\n\nwrite_files:\n  - path: /etc/wireguard/wg0.conf\n    content: |\n      [Interface]\n      PrivateKey = $7\n      ListenPort = 13231\n      Address = 172.16.16.1/24\n\n      [Peer]\n      PublicKey = $8\n      AllowedIPs = 172.16.16.90/32\n    permissions: '0600'\n\n  - path: /etc/systemd/system/wg-quick@.service.d/override.conf\n    content: |\n      [Unit]\n      Before=sshd.service\n      \n      [Service]\n      Type=oneshot\n      RemainAfterExit=yes\n    permissions: '0644'\n\n  - path: /etc/ssh/sshd_config\n    content: |\n      Port 22\n      Protocol 2\n      PermitRootLogin no\n      PubkeyAuthentication yes\n      PasswordAuthentication no\n      ChallengeResponseAuthentication no\n      UsePAM no\n      X11Forwarding no\n      AllowTcpForwarding no\n      AllowAgentForwarding no\n      PermitTunnel no\n      PrintMotd no\n      AcceptEnv LANG LC_*\n      AllowUsers $5\n      ListenAddress 172.16.16.1\n\nruncmd:\n  - usermod -p '*' $5\n  - systemctl daemon-reload\n  - systemctl enable wg-quick@wg0\n  - systemctl start wg-quick@wg0\n  - systemctl restart ssh\n  - reboot\n"
+  "user_data": "#cloud-config\nusers:\n  - name: ${user_name}\n    sudo: ['ALL=(ALL) NOPASSWD:ALL']\n    shell: /bin/bash\n    ssh_authorized_keys:\n      - \"${SSH_KEY}\"\n\npackage_upgrade: true\npackages:\n  - wireguard\n  - podman\n\nwrite_files:\n  - path: /etc/wireguard/wg0.conf\n    content: |\n      [Interface]\n      PrivateKey = ${WG_PRIV_KEY}\n      ListenPort = 13231\n      Address = 172.16.16.1/24\n\n      [Peer]\n      PublicKey = ${WG_PUB_KEY}\n      AllowedIPs = 172.16.16.90/32\n    permissions: '0600'\n\n  - path: /etc/systemd/system/wg-quick@.service.d/override.conf\n    content: |\n      [Unit]\n      Before=sshd.service\n      \n      [Service]\n      Type=oneshot\n      RemainAfterExit=yes\n    permissions: '0644'\n\n  - path: /etc/ssh/sshd_config\n    content: |\n      Port 22\n      Protocol 2\n      PermitRootLogin no\n      PubkeyAuthentication yes\n      PasswordAuthentication no\n      ChallengeResponseAuthentication no\n      UsePAM no\n      X11Forwarding no\n      AllowTcpForwarding no\n      AllowAgentForwarding no\n      PermitTunnel no\n      PrintMotd no\n      AcceptEnv LANG LC_*\n      AllowUsers ${user_name}\n      ListenAddress 172.16.16.1\n\nruncmd:\n  - usermod -p '*' ${user_name}\n  - systemctl daemon-reload\n  - systemctl enable wg-quick@wg0\n  - systemctl start wg-quick@wg0\n  - systemctl restart ssh\n  - reboot\n"
 }
 EOF
 )
@@ -39,7 +33,7 @@ EOF
 # Execute curl command
 response=$(curl -s -X POST \
   -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $9" \
+  -H "Authorization: Bearer ${API_TOKEN}" \
   -d "$payload" \
   "$API_URL")
 
