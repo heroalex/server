a9c56773ddadc7ba154b28defbceb87bbc5ff08a
alex <alex@eyesqubes.com>
2024-11-26 18:22:44 +0100

    fix init userhome paths

--- scripts/init_userhome.sh
+++ scripts/init_userhome.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-git clone https://github.com/heroalex/server.git
-
-find server/userhome/. -type f -name '*' -exec bash -c 'cp "$0" /home/${USER_NAME}/ && chown ${USER_NAME}:${USER_NAME} "/home/${USER_NAME}/$(basename "$0")";' {} \;
+find ~/server/userhome/. -type f -name '*' -exec bash -c 'cp "$0" /home/${USER_NAME}/ && chown ${USER_NAME}:${USER_NAME} "/home/${USER_NAME}/$(basename "$0")";' {} \;
