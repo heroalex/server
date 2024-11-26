094a9d9483bb2469c263cc6ea8ec19948ad596dc
alex <alex@eyesqubes.com>
2024-11-26 15:02:06 +0100

    put cloud-config in own file

--- scripts/create_server.sh
+++ scripts/create_server.sh
@@ -4,31 +4,10 @@
 API_URL="https://api.hetzner.cloud/v1/servers"
 
 # Construct JSON payload
-payload=$(cat <<EOF
-{
-  "automount": false,
-  "datacenter": "nbg1-dc3",
-  "firewalls": [
-    {
-      "firewall": ${FW_ID}
-    }
-  ],
-  "image": "ubuntu-22.04",
-  "name": "${server_name}",
-  "public_net": {
-    "enable_ipv4": true,
-    "enable_ipv6": false,
-    "ipv4": ${IP_ID}
-  },
-  "server_type": "cx22",
-  "ssh_keys": [
-    ${SSH_ID}
-  ],
-  "start_after_create": true,
-  "user_data": "#cloud-config\nusers:\n  - name: ${USER_NAME}\n    sudo: ['ALL=(ALL) NOPASSWD:ALL']\n    shell: /bin/bash\n    ssh_authorized_keys:\n      - \"${SSH_KEY}\"\n\npackage_upgrade: true\npackages:\n  - wireguard\n  - podman\n\nwrite_files:\n  - path: /etc/wireguard/wg0.conf\n    content: |\n      [Interface]\n      PrivateKey = ${WG_PRIV_KEY}\n      ListenPort = 13231\n      Address = 172.16.16.1/24\n\n      [Peer]\n      PublicKey = ${WG_PUB_KEY}\n      AllowedIPs = 172.16.16.90/32\n    permissions: '0600'\n\n  - path: /etc/systemd/system/wg-quick@.service.d/override.conf\n    content: |\n      [Unit]\n      Before=sshd.service\n      \n      [Service]\n      Type=oneshot\n      RemainAfterExit=yes\n    permissions: '0644'\n\n  - path: /etc/ssh/sshd_config\n    content: |\n      Port 22\n      Protocol 2\n      PermitRootLogin no\n      PubkeyAuthentication yes\n      PasswordAuthentication no\n      ChallengeResponseAuthentication no\n      UsePAM no\n      X11Forwarding no\n      AllowTcpForwarding no\n      AllowAgentForwarding no\n      PermitTunnel no\n      PrintMotd no\n      AcceptEnv LANG LC_*\n      AllowUsers ${USER_NAME}\n      ListenAddress 172.16.16.1\n\nruncmd:\n  - usermod -p '*' ${USER_NAME}\n  - systemctl daemon-reload\n  - systemctl enable wg-quick@wg0\n  - systemctl start wg-quick@wg0\n  - systemctl restart ssh\n  - reboot\n"
-}
-EOF
-)
+payload=$(envsubst < "cloud-config.yaml" | sed ':a;N;$!ba;s/\n/\\n/g')
+
+echo "Payload:"
+echo "$payload"
 
 # Execute curl command
 response=$(curl -s -w "\n%{http_code}" \
