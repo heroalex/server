186b58f85a06473a6f9a079c745112d2dc46e710
alex <alex@eyesqubes.com>
2024-11-27 20:50:03 +0100

    fix init script

--- scripts/init_userhome.sh
+++ scripts/init_userhome.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find /root/server/userhome/. -type f -name '*' -exec bash -c 'cp "$0" /home/${USER_NAME}/ && chown ${USER_NAME}:${USER_NAME} "/home/${USER_NAME}/$(basename "$0")";' {} \;
+find /root/server/userhome/. -type f -name '*' -exec bash -c 'cp "$0" /home/$1/ && chown $1:$1 "/home/$1/$(basename "$0")";' {} \;
