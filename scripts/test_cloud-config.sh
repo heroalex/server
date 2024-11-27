b2f63718723210e01b653357edb0a168118f776a
alex <alex@eyesqubes.com>
2024-11-27 20:26:23 +0100

    test script for lxc cloud init

--- scripts/test_cloud-config.sh
+++ scripts/test_cloud-config.sh
@@ -5,8 +5,10 @@ SSH_KEY=sshkey
 WG_PRIV_KEY=wg_priv_key
 WG_PUB_KEY=wg_pub_key
 
+cloud_config=$(envsubst < "server/scripts/cloud-config.yaml")
+
 lxd init --minimal
 lxc init ubuntu:22.04 u1
-lxc config set u1 user.user-data - < $(envsubst < "server/scripts/cloud-config.yaml")
+lxc config set u1 user.user-data - < $cloud_config
 lxc start u1
 lxc exec u1 -- bash
