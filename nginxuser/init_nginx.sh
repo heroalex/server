ef273d2fed2f7b2fc1b18be55932792ece64f96c
alex <alex@eyesqubes.com>
2024-12-01 00:59:16 +0100

    problems with nginx container

--- nginxuser/init_nginx.sh
+++ nginxuser/init_nginx.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-mkdir -p ~/.bashrc.d
-
-# Get actual user ID
-USER_ID=$(id -u)
-
-# Create runtime directories
-mkdir -p /run/user/$USER_ID/containers
-mkdir -p ~/.local/share/containers/storage
-
-# Set up environment variables
-cat > ~/.bashrc.d/systemd <<EOF
-export XDG_RUNTIME_DIR=/run/user/$USER_ID
-export RUNROOT=/run/user/$USER_ID/containers
-EOF
-
-source ~/.bashrc.d/systemd
-
-# Set proper permissions
-chmod 700 /run/user/$USER_ID
-chmod 700 /run/user/$USER_ID/containers
-
-# Reload systemd
+podman login docker.io
 systemctl --user daemon-reload
 systemctl --user start nginx
