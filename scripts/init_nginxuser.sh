8ce0261fa153924649005236bc8be2aa97f0e3a7
alex <alex@eyesqubes.com>
2024-11-30 19:31:49 +0100

    problems with nginx container

--- scripts/init_nginxuser.sh
+++ scripts/init_nginxuser.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-find /root/server/$1 -type f -exec bash -c 'echo "Processing: $0"; cp "$0" "/home/$1/" && chown "$1:$1" "/home/$1/$(basename "$0")";' {} "$1" \;
+
+cp -r /root/server/$1/* /home/$1/
+chown -R $1:$1 /home/$1/
 chmod +x /home/$1/init_nginx.sh
