b95dcc5da9e511020703bb7b23dc3652b7a23852
alex <alex@eyesqubes.com>
2024-11-30 19:46:27 +0100

    problems with nginx container

--- nginxuser/init_nginx.sh
+++ nginxuser/init_nginx.sh
@@ -5,4 +5,4 @@ mkdir -p ~/.bashrc.d
 echo "export XDG_RUNTIME_DIR=/run/user/$(id -u)" > ~/.bashrc.d/systemd
 source ~/.bashrc.d/systemd
 systemctl --user daemon-reload
-systemctl --user status nginx
+#systemctl --user enable nginx
