2ceef8d3f838e7a1245e5509faa66a2da4f6fc05
alex <alex@eyesqubes.com>
2024-12-15 19:42:51 +0100

    samba

---
+++ scripts/init_samba.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# Create Samba configuration
+echo "*** create Samba container configuration"
+cat > /etc/containers/systemd/samba-share.container << EOF
+[Unit]
+Description=Samba Share Container
+After=network-online.target media-cam1.mount media-cam2.mount
+Wants=network-online.target
+Requires=media-cam1.mount media-cam2.mount
+
+[Container]
+Image=docker.io/dperson/samba:latest
+ContainerName=samba-share
+Volume=/media/cam1:/share/cam1:Z
+Volume=/media/cam2:/share/cam2:Z
+#Network=host
+Environment=SHARE="cam1;/share/cam1;yes;no;yes;all;all;all cam2;/share/cam2;yes;no;yes;all;all;all"
+Environment=WORKGROUP=WORKGROUP
+Environment=NMBD=true
+#UserNS=keep-id
+
+[Service]
+Restart=no
+TimeoutStartSec=900
+
+[Install]
+WantedBy=default.target
+EOF
+
+echo "*** start Samba container"
+systemctl daemon-reload
+systemctl start samba-share
